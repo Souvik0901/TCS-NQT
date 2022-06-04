ee18a0f95e792c89599f354530aa5ee5754c29ad
Souvik Sen <88578056+hr-souvik@users.noreply.github.com>
2022-06-04 08:55:54 +0530

    Create 12.RemoveDuplicatesFromUnSortedArray.cpp

---
+++ Arrays/12.RemoveDuplicatesFromUnSortedArray.cpp
@@ -0,0 +1,26 @@
+
+                              Time Complexity; 0(N)
+                                Space : 0(N)
+#include<bits/stdc++.h>
+using namespace std;
+
+void RemoveDupli(int arr[], int n)
+{
+    map<int,int>mp;
+    for(int i=0; i<n; i++)
+    {
+       if( mp.find(arr[i])==mp.end())
+        { 
+            cout<<arr[i]<<" ";
+            mp[arr[i]]++;
+        }
+    }
+}
+
+
+int main()
+{
+      int a[]={6,9,0,4,5,6,4,6,6,0};
+      int n= sizeof(a)/sizeof(a[0]);
+      RemoveDupli(a,n);
+}
