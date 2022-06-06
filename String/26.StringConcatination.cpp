3e3c50e8940b346bf9adb0972bcd44a7964eda31
Souvik Sen <88578056+hr-souvik@users.noreply.github.com>
2022-06-06 08:18:36 +0530

    Create 26.StringConcatination.cpp

---
+++ String/26.StringConcatination.cpp
@@ -0,0 +1,15 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    string str1="Cristiano";
+    string  str2="Ronaldo";
+
+    cout<<str1+str2<<endl;
+    cout<<str1.append(str2)<<endl;
+    
+
+}
