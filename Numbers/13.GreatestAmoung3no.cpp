0ed7b3e8f16f27a08ce99bea42616116df0c7569
Souvik Sen <88578056+hr-souvik@users.noreply.github.com>
2022-06-09 08:16:33 +0530

    Create 13.GreatestAmoung3no.cpp

---
+++ Numbers/13.GreatestAmoung3no.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    int n1=0;
+    int n2=8, n3=18;
+    int k=max(n1,(n2,n3));
+    cout<<"Maximum  " <<k;
+}
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    int n1=0;
+    int n2=8, n3=18;
+    if(n1>n2 and n1>n3)
+    cout<<"greater"<<n1;
+    else if(n2>n1 and n2>n3)
+    cout<<"Gretetr"<<n2;
+    else  
+    cout<<"Maximum  " <<n3;
+}
+
