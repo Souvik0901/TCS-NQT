0a21dfb7a7d5ffc1e95b86b219cee3494bcf6e3c
Souvik Sen <88578056+hr-souvik@users.noreply.github.com>
2022-07-22 08:25:31 +0530

    Create 19.Power Of a Number.cpp

---
+++ Numbers/19.Power Of a Number.cpp
@@ -0,0 +1,27 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   int p=7;
+   int k=4;
+   cout<<pow(7,4);
+}
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n =3;
+    int ans=1;
+    int number=7;
+    for(int i=0; i<n; i++)
+   {
+     ans = ans*number;
+   }
+   cout<<ans;
+}
