14cdbadc030c5911d3076f52232a0117a02866af
Souvik Sen <88578056+hr-souvik@users.noreply.github.com>
2022-07-24 17:24:43 +0530

    Update 21.Prime Factor.cpp

--- Numbers/21.Prime Factor.cpp
+++ Numbers/21.Prime Factor.cpp
@@ -1,5 +1,8 @@
 
-
+Approach 1:
+    Time Complexity : 0(N)
+    Space Complexity: 0(1)
+       
 #include<bits/stdc++.h>
 using namespace std;
 void solve(int n)
@@ -23,3 +26,40 @@ int main()
     int n =12;
     solve(n);
 }
+
+
+
+
+
+Approach 2: Optimized Solution 
+
+    Time Complexity: 0(sqrt(N))
+    Space Complexity: 0(1)
+       
+       
+  #CODE:
+       
+ #include<bits/stdc++.h>
+using namespace std;
+void solve(int n)
+{
+   for(int i=2; i<=sqrt(n),i>1; i++)
+   {
+       if(n%i==0)
+       {
+            while(n%i==0)
+            {
+                cout<<i<<" ";
+                n = n/i;
+            }
+       }
+   }
+}
+
+
+int main()
+{
+    int n =78;
+    solve(n);
+}
+
