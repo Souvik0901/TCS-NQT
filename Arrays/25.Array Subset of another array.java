074f6300c795c043f71eddac917020666137ebb0
Souvik Sen <88578056+hr-souvik@users.noreply.github.com>
2022-07-31 10:11:35 +0530

    Create 25.Array Subset of another array.java

---
+++ Arrays/25.Array Subset of another array.java
@@ -0,0 +1,21 @@
+
+class Compute {
+    public String isSubset( long a1[], long a2[], long n, long m) {
+        
+        Arrays.sort(a1);
+        Arrays.sort(a2);
+        
+        int i=0;
+        int j =0;
+        while(i<n && j<m)
+        {
+            if(a1[i]==a2[j])
+               j++;
+            i++;
+        }
+        if(j==m)
+          return "Yes";
+        else
+          return "No";
+    }
+}
