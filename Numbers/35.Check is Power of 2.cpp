30029188c4028a7222b0afbbcc01a0911086d9a2
Souvik Sen <88578056+hr-souvik@users.noreply.github.com>
2022-07-31 22:16:42 +0530

    Update 35.Check is Power of 2.cpp

--- Numbers/35.Check is Power of 2.cpp
+++ Numbers/35.Check is Power of 2.cpp
@@ -1,5 +1,34 @@
 
+>> Solution : Using Recursion
 
+#include<bits/stdc++.h>
+using namespace std;
+
+bool ispower(int N)
+{
+   if(N==0) return false;
+   if(N==1) return true;
+   if(N%2!=0) return false;
+
+   return (ispower(N/2));
+}
+
+int main()
+{
+   int N=64;
+   if(ispower(N))
+   cout<<"Yes It's a power of 2";
+   else
+   cout<<"No It's not a power of 2";
+}
+
+
+
+
+
+
+>> GFG : Is it Power of 4?
+    
 class Solution
 {
     public:
