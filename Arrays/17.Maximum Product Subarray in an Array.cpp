2d411a16b353d8fcfaa48a8ed6979825e022fb84
Souvik Sen <88578056+hr-souvik@users.noreply.github.com>
2022-08-08 21:59:17 +0530

    Update 17.Maximum Product Subarray in an Array.cpp

--- Arrays/17.Maximum Product Subarray in an Array.cpp
+++ Arrays/17.Maximum Product Subarray in an Array.cpp
@@ -34,3 +34,43 @@ int main()
    int ans = MaxProd(arr,n);
    cout<<ans;
 }
+
+
+
+
+>> Brute Force Approach (Optimized Solution) : 2
+      Time Complexity :0(N*N)
+      Space Complexity :0(1)
+
+>> Code:
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int MaxProductSubArray(vector<int>nums)
+{
+  int res= INT_MIN;
+  for(int i=0; i<nums.size(); i++)
+  {
+    int prod =1;
+    for(int j=i+1; j<nums.size(); j++)
+    {
+        prod*=nums[j];
+        res= max(res,prod);
+        
+    }
+      res=  max(res, prod); //  to manages (n-1)th index of array.
+  }
+  return res;
+}
+
+
+int main()
+{
+   vector<int>arr={2,-7,-3,-1,5,6,0,2};
+   int res = MaxProductSubArray(arr);
+   cout<<res;
+}
+
+
+
