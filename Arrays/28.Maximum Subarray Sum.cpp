fe01c171e9942d945f858e7125852cc45eb5b732
Souvik Sen <88578056+hr-souvik@users.noreply.github.com>
2022-08-09 14:25:12 +0530

    Update 28.Maximum Subarray Sum.cpp

--- Arrays/28.Maximum Subarray Sum.cpp
+++ Arrays/28.Maximum Subarray Sum.cpp
@@ -61,3 +61,29 @@ int main()
 }
 
 
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int MaxSumSubArr(vector<int>arr)
+{
+   int res =INT_MIN;
+   int sum =0;
+   for(int i=0; i<arr.size();i++)
+   {
+      sum+=arr[i];
+      res = max(sum, res);
+
+      if(sum<0) sum =0;
+   }
+   return res;
+}
+
+int main()
+{
+    vector<int>arr={-1,-3,-3,9,-5,6};
+    int res =MaxSumSubArr(arr);
+    cout<<res;
+
+}
