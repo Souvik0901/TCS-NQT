b5df7bd06077ad0b85f7c14b7bcf1ae942e6824e
Souvik Sen <88578056+hr-frank@users.noreply.github.com>
2022-12-24 00:56:04 +0530

    Update 12.RemoveDuplicatesFromUnSortedArray.cpp
    https://practice.geeksforgeeks.org/problems/remove-duplicates-from-unsorted-array4141/1

--- Arrays/12.RemoveDuplicatesFromUnSortedArray.cpp
+++ Arrays/12.RemoveDuplicatesFromUnSortedArray.cpp
@@ -24,3 +24,57 @@ int main()
       int n= sizeof(a)/sizeof(a[0]);
       RemoveDupli(a,n);
 }
+
+
+GFG Solution
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+    public:
+    vector<int> removeDuplicate(int A[], int N) 
+    {
+        vector<int>ans;
+        unordered_map<int,int>mp;
+        for(int i=0; i<N; i++){
+        if(mp.find(A[i])!=mp.end())
+        continue;
+        ans.push_back(A[i]);
+        mp[A[i]]++;
+        
+        
+        }
+        return ans;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver method to test above method
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int a[n];
+        for (i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        Solution ob;
+        vector<int> v = ob.removeDuplicate(a, n);
+
+        for (auto it : v) cout << it << " ";
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
